50daebff66465498baf306e69429f918fad2fc52
Rachyabv <rachyabv@gmail.com>
2022-12-12 12:33:51 -0800

    dinning

---
+++ index.js
@@ -0,0 +1,33 @@
+let action = "we are tired";
+let otherwise = "we wake up";
+
+if (action) 
+{ 
+    console.log("we go to bed");
+}else if (otherwise) 
+{
+    console.log("start our day");
+}else{
+    console.log("read your book");
+}
+
+function getMilk() {   
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+}
